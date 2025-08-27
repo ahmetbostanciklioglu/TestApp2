575609b2c41ffbf4b35b845fa7cdc2ab35ba4a86
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-08-27 18:00:26 +0300

    Initial Commit

---
+++ TestApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestApp2
+//
+//  Created by Ahmet Bostancıklıoğlu on 27.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestApp2/TestApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp2App.swift
+//  TestApp2
+//
+//  Created by Ahmet Bostancıklıoğlu on 27.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
